0b45b2111a59caad63652a49c645c1d8a8102599
Alexander Chip <alexchip32@gmail.com>
2020-03-19 18:34:09 -0600

    Aplicando IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,17 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, plugins: [
-  new AutoPlay
+  new AutoPlay,
+  new AutoPause(),
 ] });
 
 // obteniendo los botones de controles
 const play = document.getElementById('play');
-const mute = document.getElementById('mute')
+const mute = document.getElementById('mute');
 
 
 play.onclick = () => player.togglePlay();
-mute.onclick = () => player.toggleMuted()
+mute.onclick = () => player.toggleMuted();
