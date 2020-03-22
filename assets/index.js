f668a4b16c08f07570f2608e978ef67699695234
Alexander Chip <alexchip32@gmail.com>
2020-03-22 11:24:59 -0600

    Implementando Service Worker

--- assets/index.js
+++ assets/index.js
@@ -15,3 +15,9 @@ const mute = document.getElementById('mute');
 
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMuted();
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
